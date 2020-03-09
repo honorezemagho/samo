9ee53dd85110f750ac028241b1e1a4dd6eb12468
honore237 <zankafred@gmail.com>
2020-03-09 15:40:27 +0100

    config/adminlte.php modified and Laravel Mix PureCss Package added

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,9 +14,9 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'SALEM MONEY',
     'title_prefix' => '',
-    'title_postfix' => '',
+    'title_postfix' => 'Mon Portefeuille Electronique à Moi',
 
     /*
     |--------------------------------------------------------------------------
@@ -30,12 +30,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>SALEM</b>MONEY',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image-xl',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'SAMO',
 
     /*
     |--------------------------------------------------------------------------
